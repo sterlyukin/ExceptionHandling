81a306617714aa7158f5ad9e41908481dcf361e1
sterlyukin <sterlyukinnikita@gmail.com>
2022-11-25 22:29:33 +0600

    Implemented package

---
+++ src/ExceptionHandling/ErrorResult.cs
@@ -0,0 +1,10 @@
+﻿using System.Net;
+
+namespace ExceptionHandling;
+
+public sealed record ErrorResult
+{
+    public HttpStatusCode Code { get; init; }
+    public string? TraceId { get; internal init; }
+    public IDictionary<string, string[]> Errors { get; internal init; } = default!;
+}
---
+++ src/ExceptionHandling/ServiceCollectionExtensions.cs
@@ -0,0 +1,71 @@
+﻿using System.Diagnostics;
+using System.Net;
+using System.Text.Json;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+
+namespace ExceptionHandling;
+
+public static class ServiceCollectionExtensions
+{
+    private static readonly string DefaultErrorMessage = "See logs for details";
+
+    private static Dictionary<Type, HttpStatusCode> errors = new();
+
+    public static void AddExceptionHandling<TException>(
+        this IApplicationBuilder app,
+        HttpStatusCode code)
+    where TException : Exception
+    {
+        if (app is null)
+            throw new ArgumentNullException(nameof(app));
+
+        errors.Add(typeof(TException), code);
+        
+        app.UseExceptionHandler(appError =>
+        {
+            appError.Run(async context =>
+            {
+                var exception = context.Features
+                    .Get<IExceptionHandlerPathFeature>()
+                    ?.Error;
+                if (exception is not null)
+                {
+                    if (errors.TryGetValue(exception.GetType(), out HttpStatusCode errorResult))
+                    {
+                        await ConfigureExceptionResponse(context, errorResult, exception.Message);
+                        return;
+                    }
+
+                    await ConfigureExceptionResponse(context, HttpStatusCode.InternalServerError, exception.Message);
+                }
+            });
+        });
+    }
+    
+    private static async Task ConfigureExceptionResponse(HttpContext context, HttpStatusCode code, string message)
+    {
+        var errorMessage = ConfigureErrorMessage(code, message);
+        
+        context.Response.ContentType = System.Net.Mime.MediaTypeNames.Application.Json;
+        context.Response.StatusCode = (int)code;
+
+        var errorResult = new ErrorResult
+        {
+            Code = code,
+            TraceId = Activity.Current?.TraceId.ToString(),
+            Errors = new Dictionary<string, string[]>
+            {
+                { nameof(message), new[] { errorMessage } }
+            }
+        };
+                        
+        await context.Response.WriteAsync(JsonSerializer.Serialize(errorResult));
+    }
+
+    private static string ConfigureErrorMessage(HttpStatusCode code, string message)
+    {
+        return code is HttpStatusCode.InternalServerError ? DefaultErrorMessage : message;
+    }
+}
